b8c4d1ae52eb9f9184d0d6f6e1ec2a0c08d4289b
JonathanOzsvath <ozsvathjonathan@gmail.com>
2020-01-20 19:18:50 +0100

    Javítások

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,5 @@
-$(document).ready(function () {
-    $("#flag_hu").click(function () {
-        var csrftoken = Cookies.get('csrftoken');
-        var baseUrl = window.location.protocol + "//" + window.location.host;
-        $.post(baseUrl + "/i18n/setlang/", {csrfmiddlewaretoken: csrftoken, language: "hu"})
-            .done(function (msg) {
-                console.log("done!");
-                location.reload();
-            }).fail(function (xhr, status, error) {
-                console.log(error);
-        });
-    });
-
-    $("#flag_en").click(function () {
-        var csrftoken = Cookies.get('csrftoken');
-        var baseUrl = window.location.protocol + "//" + window.location.host;
-        $.post(baseUrl + "/i18n/setlang/", {csrfmiddlewaretoken: csrftoken, language: "en"})
-            .done(function (msg) {
-                console.log("done!");
-                location.reload();
-            }).fail(function (xhr, status, error) {
-                console.log(error);
-        });
-    });
-});
+function setLang(lang) {
+    $('#langForm select').val(lang);
+    $('#langForm').submit();
+    return false;
+}
